b7190ade9a2ce9afb0f85d9d44d66621c0d1793f
Pablo Díaz <fcpauldiaz@users.noreply.github.com>
2018-02-08 17:58:17 -0600

    Update structure.js

--- src/structure.js
+++ src/structure.js
@@ -45,7 +45,7 @@ module.exports = function structure(data, meta) {
 
     field_meta.forEach(function(f, i) {
         // field name
-        f.name.split('').slice(0, 8).forEach(function(c, x) {
+        f.name.split('').slice(0, 10).forEach(function(c, x) {
             view.setInt8(32 + i * 32 + x, c.charCodeAt(0));
         });
         // field type
@@ -84,6 +84,11 @@ module.exports = function structure(data, meta) {
                         lib.lpad(val.toString(), f.size, ' ').substr(0, 18),
                         offset);
                     break;
+               case 'I':
+                offset = lib.writeField(view, f.size,
+                    lib.lpad(val.toString(), f.size, ' ').substr(0, 4),
+                    offset);
+                break;
 
                 // string
                 case 'C':
